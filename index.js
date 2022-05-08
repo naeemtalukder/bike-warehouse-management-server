0fa4b56906bfa48b96b217cc2977344fb5d4ef7e
Md Naeem talukder <naeemtalukder169135@gmail.com>
2022-05-08 16:59:44 +0600

    fetech product  url

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
@@ -25,6 +25,13 @@ async function run() {
             const products = await cursor.toArray();
             res.send(products);
         });
+
+        app.get('/product/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const product = await productCollection.findOne(query);
+            res.send(product);
+        })
     }
     finally {
 
